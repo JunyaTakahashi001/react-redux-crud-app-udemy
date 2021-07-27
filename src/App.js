6ecf921f211e9bc93b1411a0ee192faa9cad5359
*2lIAdsYiQz <j.takahashi@stageinc.jp>
2021-07-27 18:45:35 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
 // FunctionComponent
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 }
-  ]
-  return (
-    <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
-    </div>
-  )
-}
+const App = () => (<Counter></Counter>)
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
-}
+class Counter extends Component {
+  // インスタンスが作成される際、一番最初に呼び出される
+  constructor(props) {
+    super(props)
+    this.state = {count: 0}
+  }
 
-// 型チェック
-User.propTypes = {
-  name: PropTypes.string,
-  // isRequiredは、必ずage:がなくてはならないという型定義
-  age: PropTypes.number.isRequired
+  handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+  handleMinusButton = () => {
+    this.setState({ count: this.state.count - 1 })
+  }
+
+  render () {
+    return (
+      <React.Fragment>
+        <div>count: { this.state.count }</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
